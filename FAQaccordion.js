4510ee5df9fc969c6686700cc79824e207c999d5
tonibasai29@gmail.com <tonibasai29@gmail.com>
2025-06-22 19:43:20 +0300

    FAQaccordion

---
+++ FAQaccordion.js
@@ -0,0 +1,39 @@
+const listsBoxes = Array.from(document.querySelectorAll(".card-list__item"));
+
+listsBoxes.forEach((list) =>{
+    list.addEventListener("click", listHandler);
+});
+
+function listHandler (element) {
+    element.preventDefault();
+    let returnBox = element.target.closest(".list__item-img");
+    let returnContent = element.target.nextElementSibling;
+    
+    returnBox.classList.toggle("active");
+    if (returnBox.classList.contains("active")) {
+        returnContent.style.maxHeight = returnContent.scrollHeight + "rem";
+    } else {
+        returnContent.style.maxHeight = 0;
+    }
+};
+
+const table = document.querySelector(".card__list");
+let selectedTd;
+
+table.onclick = function(event) {
+  let td = event.target.closest('.card-list__item-items');
+
+  if (!td) return; 
+
+  if (!table.contains(td)) return;
+
+  clickBack(td);
+};
+
+function clickBack(td) {
+  if (selectedTd) { 
+    selectedTd.classList.remove('clickBack');
+  }
+  selectedTd = td;
+  selectedTd.classList.add('clickBack'); 
+};
